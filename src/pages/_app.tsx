50b7aeaebcf63507828a8b23e07d83b25ad0f2bb
xyztomi <amandipthandi@gmail.com>
2026-02-06 15:18:42 +0700

    feat: update Google Analytics integration in _app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,6 +11,18 @@ const inter = Inter({
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <div className={`${inter.variable} font-sans`}>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-EJST3J8J6X"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-EJST3J8J6X');
+      `}
+    </Script>
     <Component {...pageProps} />
   </div>
 );
